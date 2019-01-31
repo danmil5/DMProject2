791e22bf9c95f6d8ab0b75d919596a220d7bd10b
dmmiller1361 <dmmiller1361@H202R1S8.STUDENT.ad.fgcu.edu>
2019-01-31 16:25:32 -0500

    initial

---
+++ DMProject2/src/Main.java
@@ -0,0 +1,28 @@
+// Daniel Miller
+
+// Data Types and Ranges:
+// Byte: -128 to 127
+// Short: -32,768 to 32,767
+// Integer (Int): -2^31 to 2^31 - 1
+// Long: -2^63 to 2^63 - 1
+// Double: Most Decimal Values
+// Boolean: True or False Values
+// String: Non-Numerical Values
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        
+        // Initialize variables to be used
+        int int1 = 0;
+        double double1 = 0;
+        String string1 = "0";
+        boolean boolean1 = false;
+        // Create Scanner
+        Scanner scanner = new Scanner(System.in); 
+        scanner.close();
+    }
+}
